6dc709211f9155fa09cffffa8708c0c8e865daf2
ryanhormann <ryanhormann10@gmail.com>
2020-12-03 00:38:10 -0600

    Add switch statment for days of the week

--- lib.js
+++ lib.js
@@ -2,8 +2,10 @@ const caloriesUsedLabel = document.querySelector("#calories-used");
 const caloriesLeftLabel = document.querySelector("#calories-left");
 const form = document.querySelector("form");
 const inputs = document.querySelectorAll("input");
+const tds = document.querySelectorAll("td");
 
-const getDayOfWeek = new Date(Date.now()).getDate();
+// I added 1 to this so that it starts with monday, so monday has a value of 1, tuesday has a value of 2, etc.
+const getDayOfWeek = new Date(Date.now()).getDate() + 1;
 
 export const addCalories = () => {
   // TODO: add number from inputs to the correct day in the table
@@ -18,8 +20,32 @@ export const addCalories = () => {
   caloriesUsedLabel.textContent = totalCaloriesUsed;
   caloriesLeftLabel.textContent = 2000 - totalCaloriesUsed;
 
-  // if you eat more than 2000 calories, then the equation turns red
+  // Turn equation red if you eat more than 2k cal
   if (caloriesLeftLabel.textContent < 0) {
     document.querySelector("#calories").style.backgroundColor = "#ff837a";
+  }
+
+  switch (getDayOfWeek) {
+    case 1:
+      tds[0].textContent = caloriesUsed;
+      break;
+    case 2:
+      tds[1].textContent = totalCaloriesUsed;
+      break;
+    case 3:
+      tds[2].textContent = totalCaloriesUsed;
+      break;
+    case 4:
+      tds[3].textContent = totalCaloriesUsed;
+      break;
+    case 5:
+      tds[4].textContent = totalCaloriesUsed;
+      break;
+    case 6:
+      tds[5].textContent = totalCaloriesUsed;
+      break;
+    case 7:
+      tds[6].textContent = totalCaloriesUsed;
+      break;
   }
 };
