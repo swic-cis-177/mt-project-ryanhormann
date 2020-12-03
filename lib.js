fc88a8b55d601171c2e81ab4d89983c65b728beb
ryanhormann <ryanhormann10@gmail.com>
2020-12-03 00:21:37 -0600

    If more than 2k cal, the turn equation red

--- lib.js
+++ lib.js
@@ -3,8 +3,10 @@ const caloriesLeftLabel = document.querySelector("#calories-left");
 const form = document.querySelector("form");
 const inputs = document.querySelectorAll("input");
 
+const getDayOfWeek = new Date(Date.now()).getDate();
+
 export const addCalories = () => {
-  // TODO: add number from inputs to the equation and to the correct date in the table
+  // TODO: add number from inputs to the correct day in the table
   const caloriesUsed = {};
   let totalCaloriesUsed = 0;
 
@@ -15,4 +17,9 @@ export const addCalories = () => {
 
   caloriesUsedLabel.textContent = totalCaloriesUsed;
   caloriesLeftLabel.textContent = 2000 - totalCaloriesUsed;
+
+  // if you eat more than 2000 calories, then the equation turns red
+  if (caloriesLeftLabel.textContent < 0) {
+    document.querySelector("#calories").style.backgroundColor = "#ff837a";
+  }
 };
--- scripts.js
+++ scripts.js
@@ -7,28 +7,3 @@ for (let i = 0; i < inputs.length; i++) {
     addCalories();
   });
 }
-
-/*const caloriesUsedLabel = document.querySelector("#calories-used");
-const caloriesLeftLabel = document.querySelector("#calories-left");
-
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-
-  const inputs = document.querySelectorAll("input");
-  console.log(inputs.length);
-
-  const caloriesUsed = {};
-  let totalCaloriesUsed = 0;
-  for (let i = 0; i < inputs.length; i++) {
-    caloriesUsed[e.target.elements[i].id] = parseInt(
-      e.target.elements[i].value
-    );
-    totalCaloriesUsed += caloriesUsed[e.target.elements[i].id];
-  }
-  console.log(caloriesUsed);
-  console.log(totalCaloriesUsed);
-
-  caloriesUsedLabel.textContent = totalCaloriesUsed;
-
-  caloriesLeftLabel.textContent = 2000 - totalCaloriesUsed;
-});*/
