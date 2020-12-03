00d2833ca8636115b2520db52faca337680a1092
ryanhormann <ryanhormann10@gmail.com>
2020-12-03 00:03:45 -0600

    Create function to add calories to equation at the top

--- lib.js
+++ lib.js
@@ -1,3 +1,18 @@
+const caloriesUsedLabel = document.querySelector("#calories-used");
+const caloriesLeftLabel = document.querySelector("#calories-left");
+const form = document.querySelector("form");
+const inputs = document.querySelectorAll("input");
+
 export const addCalories = () => {
   // TODO: add number from inputs to the equation and to the correct date in the table
+  const caloriesUsed = {};
+  let totalCaloriesUsed = 0;
+
+  for (let i = 0; i < inputs.length; i++) {
+    caloriesUsed[form.elements[i].id] = parseInt(form.elements[i].value);
+    totalCaloriesUsed += caloriesUsed[form.elements[i].id];
+  }
+
+  caloriesUsedLabel.textContent = totalCaloriesUsed;
+  caloriesLeftLabel.textContent = 2000 - totalCaloriesUsed;
 };
